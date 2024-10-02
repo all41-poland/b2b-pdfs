e51a8cdb40c3b242a34efa34f664a40c16022aca
DJ-Chemik <dj-chemik@wp.pl>
2024-10-02 16:22:10 +0200

    Add new input layout

--- renderer/pages/_app.tsx
+++ renderer/pages/_app.tsx
@@ -1,7 +1,25 @@
 import './global.css';
+import '@mantine/core/styles.css';
+import type { AppProps } from 'next/app';
+import { createTheme, MantineProvider, Input as MantineInput } from '@mantine/core';
+import styles from './app.module.css';
 
-const GlobalApp = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+const theme = createTheme({
+  components: {
+    InputWrapper: MantineInput.Wrapper.extend({
+      classNames: {
+        label: styles.label,
+      },
+    }),
+  },
+});
+
+const GlobalApp = ({ Component, pageProps }: AppProps) => {
+  return (
+    <MantineProvider theme={theme}>
+      <Component {...pageProps} />
+    </MantineProvider>
+  );
 }
 
 export default GlobalApp;
